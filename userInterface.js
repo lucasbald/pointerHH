58b8ad502dbffd3c737b52c91daccbbe18cbc7fd
Lucas <lucasbaldin1311@gmail.com>
2019-07-13 12:44:16 -0300

    Added user interface elements

---
+++ userInterface.js
@@ -0,0 +1,30 @@
+var read = require('./lib/read.js')
+const fs    = require('fs')
+
+
+function prompt() {
+	read({ prompt: 'Username: ', default: 'test-user' }, function (er, user) {
+		read({ prompt: 'Password: ', default: 'test-pass', silent: true }, function (er, pass) {
+			read({ prompt: 'Password again: ', default: 'test-pass', silent: true }, function (er, pass2) {
+				const verification = (pass === pass2)
+				const userCredentials = {
+					user,
+					pass
+				}
+
+				if (verification) {
+					if (!fs.existsSync('user.secret.json')) fs.writeFile('user.secret.json', JSON.stringify(userCredentials), function () {})
+					return
+				} else {
+					console.log('Password did not match, please try again!')
+					prompt()
+				}
+			})
+		})
+	})
+}
+
+
+prompt()
+
+ 
